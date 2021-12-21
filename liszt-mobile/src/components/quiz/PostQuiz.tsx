cd695019295252d1bcb499eb76f19b52c504029d
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2021-12-20 17:00:48 -0800

    Add a prebuilt chart from react-native-chart-kit

--- liszt-mobile/src/components/quiz/PostQuiz.tsx
+++ liszt-mobile/src/components/quiz/PostQuiz.tsx
@@ -1,6 +1,7 @@
 import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, Dimensions, StyleSheet, Text, View} from 'react-native';
+import {ProgressChart} from 'react-native-chart-kit';
 import {QuizMetadata} from './types';
 
 const postQuiz: React.FC<{
@@ -9,16 +10,43 @@ const postQuiz: React.FC<{
 }> = ({metadata, onReset}) => {
   const navigation = useNavigation();
 
+  metadata.accuracy = 0.2;
+
   return (
-      <View style={{padding: 24}}>
-        <Text style={{fontWeight: 'bold', fontSize: 24}}>Finished</Text>
-        <Text>Correct Questions: {metadata.totalCorrect}</Text>
-        <Text>Total Questions: {metadata.totalQuestions}</Text>
-        <Text>Accuracy: {metadata.accuracy * 100}%</Text>
-        <Text>Average response time: {metadata.averageDwellTime}s</Text>
-        <Button title="Go again" onPress={() => onReset()} />
-        <Button title="Home" onPress={() => navigation.navigate('Home')} />
+    <View style={{padding: 24}}>
+      <Text style={{fontWeight: 'bold', fontSize: 24}}>Finished</Text>
+      <Text>Correct Questions: {metadata.totalCorrect}</Text>
+      <Text>Total Questions: {metadata.totalQuestions}</Text>
+      <Text>Average response time: {metadata.averageDwellTime}s</Text>
+      <View style={{}}>
+        <ProgressChart
+          data={{labels: ['Accuracy'], data: [metadata.accuracy]}}
+          width={Dimensions.get('window').width - 24 * 2}
+          height={200}
+          strokeWidth={16}
+          radius={52}
+          hideLegend={true}
+          chartConfig={{
+            backgroundGradientFromOpacity: 0,
+            backgroundGradientToOpacity: 0,
+            color: (opacity = 0) => `rgba(98, 195, 112, ${opacity})`,
+            strokeWidth: 2, // optional, default 3
+          }}
+        />
+        <Text
+          style={{
+            top: -123,
+            left: 5,
+            fontSize: 24,
+            textAlign: 'center',
+          }}>
+          {metadata.accuracy * 100}%
+        </Text>
+        <Text style={{top: -120, left: 2, textAlign: 'center'}}>Accuracy</Text>
       </View>
+      <Button title="Go again" onPress={() => onReset()} />
+      <Button title="Home" onPress={() => navigation.navigate('Home')} />
+    </View>
   );
 };
 
